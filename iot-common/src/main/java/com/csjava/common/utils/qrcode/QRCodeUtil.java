b318d372158bde8c1a7da00965756cfe6e8a60bd
muzhen <1184321378@qq.com>
2022-08-30 09:08:09 +0800

    first commit

---
+++ iot-common/src/main/java/com/csjava/common/utils/qrcode/QRCodeUtil.java
@@ -0,0 +1,15 @@
+package com.csjava.common.utils.qrcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @title: QRCodeUtils
+ * @Author muzhen
+ * @Date: 2022/8/29 下午4:27
+ * @Version 1.0
+ */
+@Slf4j
+public class QRCodeUtil {
+
+
+}
---
+++ iot-common/src/main/java/com/csjava/common/utils/system/SystemUtil.java
@@ -0,0 +1,13 @@
+package com.csjava.common.utils.system;
+
+/**
+ * @title: SystemUtil
+ * @Author muzhen
+ * @Date: 2022/8/29 下午4:50
+ * @Version 1.0
+ */
+
+public class SystemUtil {
+
+    
+}
